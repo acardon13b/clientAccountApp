1bdcbddd11a34366ac68b0e4092f1d46d356e3d2
milaunreal <miladaudetcloud@gmail.com>
2023-07-19 03:33:53 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          v0.1
+          v0.2
         </p>
         <a
           className="App-link"
