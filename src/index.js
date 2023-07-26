e34b96fd015e3074b17040473bda88e53b6cfd16
milaunreal <miladaudetcloud@gmail.com>
2023-07-26 17:45:33 +0200

    scaffold version working

--- src/index.js
+++ src/index.js
@@ -6,8 +6,34 @@ import reportWebVitals from './reportWebVitals';
 //Start of AMplify config
 import { Amplify } from 'aws-amplify';
 import config from './aws-exports';
-Amplify.configure(config);
+
+
+let updatedAwsExports = {};
+
+
+  Amplify.Logger.LOG_LEVEL = "DEBUG";
+  updatedAwsExports = {
+    ...config,
+    oauth: {
+      ...config.oauth,
+      domain: "login.metalive.art",
+      scope: ["email", "openid"],
+      redirectSignIn: "https://main.dphac8bawa2rt.amplifyapp.com/",
+      redirectSignOut: "https://main.dphac8bawa2rt.amplifyapp.com/",
+      //redirectSignIn: "http://localhost:3000/",
+      //redirectSignOut: "http://localhost:3000/",
+      responseType: "code" // or 'token', whatever you configured
+    },
+  };
+
+
+
+Amplify.configure(updatedAwsExports);
 // Amplify config finished
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,6 +41,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
